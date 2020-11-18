0a0694ad21336d9e443fd42299146ec12890f83f
Looloo414 <alexafaulkison@gmail.com>
2020-11-17 23:48:48 -0700

    Reorganized/not complete

--- js/app.js
+++ js/app.js
@@ -1,44 +1,83 @@
 // --------------CONSTANTS----------
-
-
+const winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 // -------------VARIABLES------------
-let startGame = true;
-let xTurn = true;
-
+let board, turn, winner;
 
 // --------------CACHED ELEMENTS--------
 const cellElements = document.querySelectorAll('.cell');
 const winningMessageText = document.querySelector('.winning-message');
-
 const startOver = document.querySelector('.reset-button');
-
-
-
-
-// --------------FUNCTIONS--------------------
-const initiate = (e) => {
-    console.log(e);
-};
-
-const handleCellClick = (e) => {
-    // console.log(e.target);
-    const location = e.target.classList[1];
-
-    if(xTurn) {
-        console.log(e.target);
-    }
-}
 
 // --------------EVENT LISTENERS------------
 startOver.addEventListener('click', initiate);
 
-// for (const cellDiv of cellElements) {
-//     cellDiv.addEventListener('click', handleCellClick)
-// }
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleCellClick, { once: true})
 });
+
+
+// --------------FUNCTIONS--------------------
+initiate();
+
+function initiate() {
+    board = [ '', '', '', '', '', '', '', '', ''];
+    turn = 1;
+    winner = null;
+    render();
+}
+
+function handleClick(click) {
+    let idx = parseInt(click.target.id);
+    if (board[idx] != "null") {
+        return;
+    }
+    board[idx] = turn;
+    turn *= -1;
+    winner = theWinner(idx);
+    render();
+}
+
+function theWinner(idx) {
+    if (
+        board[0] + board[1] + board[2] === 3 ||
+        board[3] + board[4] + board[5] === 3 ||
+        board[6] + board[7] + board[8] === 3 ||
+        board[0] + board[3] + board[6] === 3 ||
+        board[1] + board[4] + board[7] === 3 ||
+        board[2] + board[5] + board[8] === 3 ||
+        board[0] + board[4] + board[8] === 3 ||
+        board[6] + board[4] + board[2] === 3 
+    ) {
+        return 1;
+    }
+    if (
+        board[3] + board[4] + board[5] === -3 ||
+        board[6] + board[7] + board[8] === -3 ||
+        board[0] + board[1] + board[2] === -3 ||
+        board[0] + board[3] + board[6] === -3 ||
+        board[1] + board[4] + board[7] === -3 ||
+        board[2] + board[5] + board[8] === -3 ||
+        board[0] + board[4] + board[8] === -3 ||
+        board[6] + board[4] + board[2] === -3 
+    ) {
+        return -1;
+    }
+}
+
+// const initiate = (e) => {
+//     console.log(e);
+// };
+
+// const handleCellClick = (e) => {
+//     // console.log(e.target);
+//     const location = e.target.classList[1];
+
+//     if(xTurn) {
+//         console.log(e.target);
+//     }
+// }
+
 
 
    
@@ -78,7 +117,7 @@ cellElements.forEach(cell => {
 
 // Variables might include (board/turn/winner)
 
-// let circleTurn
+// let oTurn
 
 /*------------------------ Cached Element References ------------------------*/
 // const cellElements = document.querySelectorAll('[data-cell]')
@@ -101,7 +140,7 @@ cellElements.forEach(cell => {
 
 
 // function initiate() {
-//     circleTurn = false;
+//     oTurn = false;
 //     cellElements.forEach(cell => {
 //         cell.classList.remove(playerO)
 //         cell.classList.remove(playerX)
@@ -111,9 +150,17 @@ cellElements.forEach(cell => {
 // };
 // initiate();
 
+// // function handleClick(e) {
+// //     let idx = parseInt(e.target.id);
+// //     if (board[idx] != 'null') {
+// //         return;
+// //     }
+// //     swapTurns()
+// // }
+
 // function handleClick(e) {
 //     const cell = e.target
-//     const currentClass = circleTurn ? playerO : playerX 
+//     const currentClass = oTurn ? playerO : playerX 
 //     placeMark(cell, currentClass)
 //     if (winner(currentClass)){
 //         endGame(false)
@@ -128,7 +175,7 @@ cellElements.forEach(cell => {
 //     if (tie) {
 //         winningMessageText.innerText = "It's a Tie!"
 //     } else {
-//         winningMessageText.innerText = `${circleTurn ? "O" : "X"} Wins!`
+//         winningMessageText.innerText = `${oTurn ? "O" : "X"} Wins!`
 //     }
 //     winningMessage.classList.add('show')
 // }
@@ -144,8 +191,13 @@ cellElements.forEach(cell => {
 
 // };
 
+// function clear() {
+//     board = [ '', '', '', '', '', '', '', '', ''];
+//     initiate();
+// }
+
 // function swapTurns() {
-//     circleTurn = !circleTurn
+//     oTurn = !oTurn
 // };
 
 // function winner(currentClass) {
@@ -155,6 +207,9 @@ cellElements.forEach(cell => {
 //         })
 //     })
 // }
+
+
+
 
 // Some functions you might choose to use:
 
